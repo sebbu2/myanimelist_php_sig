82c59429bcaac1eb40b35e1c709f1c561463c0d6
sebbu <zsbe17fr@yahoo.fr>
2015-07-03 18:50:33 +0200

    Order results alphabetically (fix some wrong detections)

--- sig.php
+++ sig.php
@@ -145,6 +145,13 @@ $count_=0;
 //$animes=search($_name,true);
 //$animes=search($_name,false);
 $animes=search2($_name,false);
+
+function animes_sort($arg1, $arg2)
+{
+	return strnatcmp($arg1['name'], $arg2['name']);
+}
+usort($animes, animes_sort);
+
 $id=-1;
 //$arr='auto,UTF-7,eucJP-win,SJIS-win,ISO-2022-JP,ISO-8859-1,ISO-8859-2,ISO-8859-3,ISO-8859-4,ISO-8859-5,ISO-8859-6,ISO-8859-7,ISO-8859-8,ISO-8859-9,ISO-8859-10,ISO-8859-11,ISO-8859-12,ISO-8859-13,ISO-8859-14,ISO-8859-15';//,UTF-16,UTF-32,UCS2,UCS4';
 $arr='UTF-8,ISO-8859-1,ISO-8859-2,ISO-8859-3,ISO-8859-4,ISO-8859-5,ISO-8859-6,ISO-8859-7,ISO-8859-8,ISO-8859-9,ISO-8859-10,ISO-8859-11,ISO-8859-12,ISO-8859-13,ISO-8859-14,ISO-8859-15,ASCII,JIS,EUC-JP,SJIS,UTF-7,eucJP-win,SJIS-win,ISO-2022-JP';//,UTF-16,UTF-32,UCS2,UCS4';
